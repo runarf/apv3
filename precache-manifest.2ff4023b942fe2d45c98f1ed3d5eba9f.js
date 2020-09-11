0ec3399b5bb3d61b16d5e78ff9ca2f2748098273
Runar <runar.flaten@zalando.de>
2020-09-11 22:30:45 +0200

    Updates

---
+++ precache-manifest.2ff4023b942fe2d45c98f1ed3d5eba9f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce394f7c551040c469026f6e7a9b7b4a",
+    "url": "/apv3/index.html"
+  },
+  {
+    "revision": "175547ac27b77c4b31db",
+    "url": "/apv3/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1c225c65c19f0eaff85a",
+    "url": "/apv3/static/js/2.2e69e72d.chunk.js"
+  },
+  {
+    "revision": "9faf4657f5b7195d3685030077bec9d5",
+    "url": "/apv3/static/js/2.2e69e72d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175547ac27b77c4b31db",
+    "url": "/apv3/static/js/main.a6571ce6.chunk.js"
+  },
+  {
+    "revision": "35fafb57999f0b4dc696e1a37110e8a3",
+    "url": "/apv3/static/js/main.a6571ce6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92e7e70cc84b6935df4",
+    "url": "/apv3/static/js/runtime-main.7019524a.js"
+  },
+  {
+    "revision": "2388621b34168ca691b4ce69adc700dd",
+    "url": "/apv3/static/media/Companero_25.2388621b.jpg"
+  },
+  {
+    "revision": "3e7a7181a2eb1e5349ae23d3ac9027e6",
+    "url": "/apv3/static/media/Dog_Chow_Adultos.3e7a7181.jpg"
+  },
+  {
+    "revision": "13beea48ea42c56972c4d4c357da55f9",
+    "url": "/apv3/static/media/Dog_Chow_Cachorros.13beea48.jpg"
+  },
+  {
+    "revision": "3abbe5328c0c20bff986bc80d062cb1e",
+    "url": "/apv3/static/media/Dog_Chow_RP_25.3abbe532.jpg"
+  },
+  {
+    "revision": "404a94e74e88d3150c669a6f78673771",
+    "url": "/apv3/static/media/El_Centinela_25.404a94e7.png"
+  },
+  {
+    "revision": "514b0496370e73f11ace3df1c26cb0e1",
+    "url": "/apv3/static/media/Flagacan.514b0496.jpg"
+  },
+  {
+    "revision": "6a1d8fbb64b4db390a176b117515db2f",
+    "url": "/apv3/static/media/Gatina_15.6a1d8fbb.jpg"
+  },
+  {
+    "revision": "d172fe11506ca1752f53a52c24bc7e24",
+    "url": "/apv3/static/media/LogoBig.d172fe11.png"
+  },
+  {
+    "revision": "56fd11b69aa5812e49252d2184d69730",
+    "url": "/apv3/static/media/Pet_Can_25.56fd11b6.png"
+  },
+  {
+    "revision": "c742fd756968eaebbf0de729264d0f01",
+    "url": "/apv3/static/media/Pet_Cat_15.c742fd75.png"
+  },
+  {
+    "revision": "7af6f61e8b6b515eb1ab5c61f73142a6",
+    "url": "/apv3/static/media/Poder_Canino_25.7af6f61e.jpg"
+  },
+  {
+    "revision": "667643f96c45c0f19899f4bb096f0eb5",
+    "url": "/apv3/static/media/X-Dog_Adulto_25.667643f9.png"
+  },
+  {
+    "revision": "7596687e9fd3aaf2f750eff34b1319f6",
+    "url": "/apv3/static/media/Xdog_Basic_25.7596687e.jpg"
+  }
+]);
